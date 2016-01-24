6c93a852dea3b0b9277b60c43b3754e8f89b51eb
Jordan OMara <jsomara@gmail.com>
2016-01-24 15:17:24 -0800

    Add data for homelands, careers, origins

--- character_data/parse.rb
+++ character_data/parse.rb
@@ -1,6 +1,6 @@
 def format_line(line)
-  line.gsub!('"', '\"''"')
+  line.gsub!('"', '\"')
   line.gsub!("\r\n", "")
   return "\"#{line}\\n\"+"
 end
-File.open('exploits.csv').each {|x| puts format_line(x) }
+File.open('homeworlds.csv').each {|x| puts format_line(x) }
--- js/controllers/components.js
+++ js/controllers/components.js
@@ -3,7 +3,14 @@ var angular = require('angular');
 angular.module('woin-character').service('Components',
     function() {
       var self = this;
-      var careers = 'testempty';
+      var careers =
+        "Career,Prerequisites,Attributes,Skill Choices,Description,Exploits,Years,Source\n"+
+        "Academy,,\"agi:1,log:1;wil:1;cha:1\",\"carrying, pistols, rifles, leadership, law, [scientific], [technical], tactics, leadership, starship tactics\",\"You joined the military and completed basic military training. Some programs send recruits to college to gain degrees before returning to cadet assignments. The Military Academy is the basic training location for both Naval and Marine officers, and as such covers a wide curriculum along with an opportunity to specialize early in a science, medicine, or engineering career. The Military Academy is regarded as a top-quality institution, easily the equal of many highly placed universities. The Academy is a three-year course, and is widely regarded as the equivalent of a Bachelor's degree.\",\"Basic Training, Command School [requires Basic Training], Branch Specialization [requires Basic Training; one [scientific] skill, Academy Tutor (requires Basic Training), Academy Professor (Requires Academy Tutor)\",3,Future\n"+
+        "Assassin,skill:stealth;skill:tracking;skill:[combat],str:1;agi:1;int:1;rep:1,\"[combat ], stealth, thievery, perception, intimidate, disguise\",\"A killer for hire, you mastered the skills of assassination.\",\"Killing Blow, Ambush, Weak Point, Sneak\",1d6,\"Modern, Future\"\n"+
+        "Archer,attribute:agi:3,str:1;agi:1;luc:1;rep:1,\"bows, perception, carousing, survival\",\"You joined the army as an archer, manning walls and front lines in times of war.\",\"Long Shot, Bowyer, Careful Aim, Rapid Shot, Stand Your Ground, Double Shot, Intercepting Shot\",1d6,Archaic \n"+
+        "Barbarian,,str:1;end:1;agi:1;int:1,\"swords, axes, spears, [physical], [outdoor], hardy, herbalism\",\"Tribal warriors from the very fringes of civilization, barbarians are wild and uncouth. Barbarians exemplify physical prowess and natural prowess, but can feel uncomfortable in enclosed spaces.\",\"Feet Of Foot, Mighty Leap, Set In The Old Ways, Primal Charge (requires Fleet of Foot), Hides & Skins, Leathery Skin, Iron Skin (requires Leathery Skin), Sacred Terrain, Beastly Visage, Scarred Visage (requires Beastly Visage, Leather skin), Keen Senses, Trophy Collection (requires Set In The Old Ways), Reap The Whirlwind, Feral, Natural Serenity (requires Sacred Terrain)\",1d6,Archaic \n"+
+        "Private Eye,,agi:1;int:1;log:1;cha:1,\"pistols, brawling, perception, persuasion, carousing, driving, thievery, stealth\",\"You snoop, bribe, tail, and occasionally get socked a few times a month in order to pay the bills. You know all the shady spots in town.\",\"Snoop, Contacts, Great Detective, Hardboiled\",1d6,Modern \n"+
+        "Driver,,agi:1;int:1;cha:1;rep:1,\"driving, perception, engineering, carousing\",,\"Getaway, Racer, Drag Racer, Evasive Driving, Shoot 'n Drive, Yee-haaa!, Defensive Driving\",1d6,Modern \n"
       var chis = 'testempty';
       var derived_stats = 'testempty';
       var equipment = 'testempty';
@@ -15,10 +22,23 @@ angular.module('woin-character').service('Components',
         "Always prepared,attribute:log:8;,\"You have a brilliant tactical mind, and are always prepared. You may declare a single action which you took prior to the encounter (even if you didn't know the encounter was going to take place) which helps you in this exact scenario. This must be an action you were capable of.\"\n"+
         "Analytics,attribute:log:6;,\"Studying your target's behaviour, you notice a pattern to its actions. You grant all allies within 30' a +1d6 bonus to attack the target until the start of your next turn.\"\n"+
         "Arcane recharge,skill:meditation:,\"You can spend five minutes to recover 2d6 Magic Points once per day.\"";
-      var homelands = 'testempty';
+      var homelands =
+        "Homeworld,Attributes,Bonus Skill\n"+
+        "Agricultural,end:1;log:-1,Farming\n"+
+        "Arctic,end:1;agi-1,Survival\n"+
+        "Asteroid,agi:1;str-1,Zero-g or mining\n"+
+        "Barren,end:1;cha-1,Survival\n"+
+        "City,cha:1;end-1,Diplomacy or bureacracy\n"+
+        "Desert,end:1;agi-1,Navigation or survival\n"+
+        "Jungle,agi:1;end:-1,Climbing\n"+
+        "Ocean,agi:1;int-1,Swimming or sailing\n"+
+        "Volcanic,agi:1;str:-1,Dodging\n";
       var hooks = 'testempty';
       var magics = 'testempty';
-      var origins = 'testempty';
+      var origins =
+        "Origin,Prerequisites,Attributes,Skill Choices,Description,Exploits,Years,Source\n"+
+        "Acolyte,none,int:1;wil:1;cha:1;luc:1,\"religion, [artistic], intuition, meditation, medicine, herbalism, linguistics\",A childhood spent in a monastery taught you well for a life of piety.,Daily Worship,2d6+6,Archaic\n"+
+        "Farmhand,none,str:1;end:1;luc:2,\"nature, herbalism, animal handling, farming, fishing, survival\",\"You grew up on a farm, learning how to manager crops and livestock.\",Outdoorsman,2d6+6,Archaic\n"
       var psionics = 'testempty';
       var races =
         "Race,Size,Attributes,Skill Choices,Exploits\n"+
@@ -63,6 +83,23 @@ angular.module('woin-character').service('Components',
           },
           complete: function () {
             console.log("Caraeers Loaded");
+          }
+        });
+
+        Papa.parse(doDownload ? getUrl('homeworlds') : homelands, {
+          header: true,
+          download: doDownload,
+          dynamicTyping: true,
+          step: function (row) {
+            var KEY = 'Homelands';
+            scope.homelands.push(row.data[0]);
+            scope.homelandHash = {};
+            _.each(scope.homelands, function (item) {
+              scope.homelandHash[item[KEY]] = item;
+            });
+          },
+          complete: function () {
+            console.log("Homelands Loaded");
           }
         });
 
