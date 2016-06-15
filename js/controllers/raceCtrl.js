96f4d272e07719222f240b716b9b6074db2aa152
seiyria <kyle@seiyria.com>
2016-06-15 14:54:08 -0500

    race exploits, closes #8

--- js/controllers/raceCtrl.js
+++ js/controllers/raceCtrl.js
@@ -2,16 +2,34 @@
 angular = require('angular');
 
 angular.module('woin-character')
-  .controller('RaceCtrl', function RaceCtrl($scope) {
+  .controller('RaceCtrl', function RaceCtrl($scope, $rootScope) {
 
     var KEY = $scope.KEY = 'Race';
     if(!$scope.character[KEY]) $scope.character[KEY] = {};
     $scope.generalHash = $scope.$parent.generalHash;
 
+    $scope.changeRace = function() {
+        var raceExploits = $scope.character.race.Exploits.split(', ');
+
+        $scope.character.exploits = $scope.character.exploits || [];
+
+        _.each($scope.character.exploits, function(exploit) {
+           if(!exploit || !_.contains($rootScope.raceExploits, exploit.Exploit)) return;
+            $scope.character.exploits = _.reject($scope.character.exploits, { Exploit: exploit.Exploit });
+        });
+
+        _.each(raceExploits, function(exploit) {
+           $scope.character.exploits.push($scope.exploitsHash[exploit]);
+        });
+
+        $rootScope.raceExploits = raceExploits;
+    };
+
     $scope.printAttributes = function(attributeString) {
-      var attributes = attributeString.split(',');
+      var attributes = (attributeString || '').split(',');
       var printString = '';
       angular.forEach(attributes, function(attr) {
+        if(!attr) return;
         var tokens = attr.split(':');
         printString += tokens[0].toUpperCase() + " +" + tokens[1] + ', ';
       });
