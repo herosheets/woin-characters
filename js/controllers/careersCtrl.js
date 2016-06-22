d2be3d2f811bf5831316794473f5e768e0d770a0
seiyria <kyle@seiyria.com>
2016-06-22 13:13:05 -0500

    can now choose one exploit per career tier chosen, closes #10

--- js/controllers/careersCtrl.js
+++ js/controllers/careersCtrl.js
@@ -2,18 +2,36 @@
 angular = require('angular');
 
 angular.module('woin-character')
-  .controller('CareersCtrl', ['ExploitService', '$scope', function CareersCtrl(ExploitService, $scope) {
+  .controller('CareersCtrl', ['ExploitService', '$rootScope', '$scope', function CareersCtrl(ExploitService, $rootScope, $scope) {
 
     var KEY = $scope.KEY = 'careers';
 
-    var deleteFromArray = function(array, c) {
-      var index = array.indexOf(c);
-      array.splice(index, 1);
-    };
-
     $scope.atMaxCareers = function() {
         return _.reduce(_.values($scope.character[KEY]), function(prev, cur) { return prev + cur; }, 0) >= 4;
-    }
+    };
+
+    $rootScope.careerExploitStrings = $rootScope.careerExploitStrings || [];
+    $rootScope.careerExploits = $rootScope.careerExploits || [];
+
+    $scope.chooseCareer = function(career) {
+        $scope.incrementItem(KEY, career.Career, 1);
+
+        $rootScope.careerExploits.push({ career: career.Career, exploit: career._currentExploit });
+
+        $scope.character.exploits = $scope.character.exploits || [];
+        $scope.character.exploits.push($scope.exploitsHash[career._currentExploit]);
+        $rootScope.careerExploitStrings.push(career._currentExploit);
+
+        career._currentExploit = null;
+    };
+
+    $scope.removeCareer = function(careerName, exploitLost) {
+        $scope.decrementItem(KEY, careerName, 1);
+
+        $rootScope.careerExploitStrings = _.reject($rootScope.careerExploitStrings, function(carObj) { return carObj.exploit === exploitLost; });
+        $scope.character.exploits = _.reject(career._currentExploit, function(car) { return car.Exploit === exploitLost; });
+        $rootScope.careerExploits = _.reject(career._currentExploit, function(car) { return car === exploitLost; });
+    };
 
     $scope.printAttributes = function(attributeString) {
       if(!attributeString) attributeString = '';
@@ -28,6 +46,7 @@ angular.module('woin-character')
     };
 
     $scope.exploitsFilteredRequirements = function(career) {
+      if(!career.Exploits) return [];
       var exploits = career.Exploits.split(',');
       return _.filter(exploits, function(exploit) {
         return ExploitService.meetsCareerRequirements($scope.character, exploit, $scope.exploitsHash);
